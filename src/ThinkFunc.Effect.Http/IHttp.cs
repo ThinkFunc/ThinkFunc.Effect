2d12c2072c5e3f3554389cc371c19d3285a07fa2
SeungYongShim <syshim@me.com>
2022-10-29 23:13:03 +0900

    .

--- src/ThinkFunc.Effect.Http/IHttp.cs
+++ src/ThinkFunc.Effect.Http/IHttp.cs
@@ -1,22 +1,10 @@
 using LanguageExt;
-using LanguageExt.Effects.Traits;
 using LanguageExt.Pipes;
 using Microsoft.AspNetCore.Http;
 using ThinkFunc.Effect.Abstractions;
 using static LanguageExt.Prelude;
-using static LanguageExt.Pipes.Proxy;
-using Microsoft.AspNetCore.Mvc;
-using FluentValidation;
-using LanguageExt.ClassInstances;
-using System.ComponentModel.DataAnnotations;
 
 namespace ThinkFunc.Effect.Http;
-
-public interface IValid<RT, T> : IHas<RT, IValidator<T>>
-    where RT : struct, IHasCancel<RT>, IValid<RT, T>
-{
-
-}
 
 public interface IHttp<RT> : IHas<RT, HttpContext>
     where RT : struct, IHasCancel<RT>, IHttp<RT>
@@ -36,18 +24,5 @@ public interface IHttp<RT> : IHas<RT, HttpContext>
                 _ => Results.StatusCode(500)
             }))
         from _2 in Aff(() => _1.ExecuteAsync(http).ToUnit().ToValue())
-        select unit;
-
-
-    public static Producer<RT, T, Unit> RequestTo<T>() =>
-       from http in IHas<RT, HttpContext>.Eff
-       from _1 in Aff(() => http.Request.ReadFromJsonAsync<T>())
-       from _2 in yield(_1)
-       select unit;
-
-    public static Pipe<RT, T, T, Unit> Validation<T>(IValidator<T> validator) =>
-        from v in awaiting<T>()
-        from _1 in Aff<RT, Unit>(rt => validator.ValidateAndThrowAsync(v).ToUnit().ToValue())
-        from _2 in yield(v)
         select unit;
 }
---
+++ src/ThinkFunc.Effect.Http/IValid.cs
@@ -0,0 +1,16 @@
+﻿using LanguageExt;
+using LanguageExt.Pipes;
+using ThinkFunc.Effect.Abstractions;
+using static LanguageExt.Prelude;
+using FluentValidation;
+
+namespace ThinkFunc.Effect.Http;
+
+public interface IValid<RT, T> : IHas<RT, IValidator<T>>
+    where RT : struct, IHasCancel<RT>, IValid<RT, T>
+{
+    public static Aff<RT, T> Validate(T dto) =>
+        from valid in IHas<RT, IValidator<T>>.Eff
+        from _1 in Aff(() => valid.ValidateAndThrowAsync(dto).ToUnit().ToValue())
+        select dto;
+}
